b7f6b1144aa8572e66bdafbc34235b3d58ce7596
Suad Suljovic <c.suad.suljovic@northvolt.com>
2021-11-21 14:23:39 +0100

    finished upload route

--- src/main/java/com/api/restservice/controller/UploadController.java
+++ src/main/java/com/api/restservice/controller/UploadController.java
@@ -1,20 +1,29 @@
 package com.api.restservice.controller;
 
-import org.springframework.http.HttpStatus;
+import com.api.restservice.model.AmazonImage;
+import com.api.restservice.service.amazonS3.AmazonS3ImageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api")
 public class UploadController {
+
+    @Autowired
+    private AmazonS3ImageService amazonS3ImageService;
     
     @PostMapping("image/upload")
-    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file){
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<AmazonImage> insertImage(@RequestPart(value = "image") MultipartFile image) {
+        return ResponseEntity.ok(getAmazonS3ImageService().uploadImageToAmazon(image));
     }
-    
+
+    public AmazonS3ImageService getAmazonS3ImageService() {
+        return amazonS3ImageService;
+    }
 }
