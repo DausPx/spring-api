ec8b06c940542fc30eb710966a8edd8b1eb42136
Suad Suljovic <c.suad.suljovic@northvolt.com>
2021-11-22 16:57:22 +0100

    added simple data for testing

--- src/main/java/com/api/restservice/RestServiceApplication.java
+++ src/main/java/com/api/restservice/RestServiceApplication.java
@@ -1,5 +1,10 @@
 package com.api.restservice;
 
+import java.util.Random;
+
+import com.api.restservice.model.Image;
+import com.api.restservice.model.Rating;
+import com.api.restservice.model.Sight;
 import com.api.restservice.repository.ImageRepository;
 import com.api.restservice.repository.RatingRepository;
 import com.api.restservice.repository.SightRepository;
@@ -19,7 +24,7 @@ public class RestServiceApplication implements CommandLineRunner{
 	ImageRepository imageRepository;
 
 	@Autowired
-	RatingRepository scoreRepository;
+	RatingRepository ratingRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
@@ -27,8 +32,26 @@ public class RestServiceApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO: add test data.
+		Image image1 = new Image("http.nesto.com");
+		Image image2 = new Image("http.nesto2.com");
+		Image image3 = new Image("http.nesto3.com");
 		
+		Sight sight1 = new Sight("ime1", "opis koji treba da je vezan za ime 1", true, "znamenito", "11.11.11", "22.22.22");
+
+		sight1.addImage(image1);
+		sight1.addImage(image2);
+		sight1.addImage(image3);
+
+		Sight createdSight = sightRepository.save(sight1);
+
+		Random random = new Random(); 
+
+		for(int i = 0; i<10; i++){
+			Rating rating = new Rating(random.nextInt(1, 6),createdSight);
+			ratingRepository.save(rating);
+		}
+
+
 	}
 
 }
